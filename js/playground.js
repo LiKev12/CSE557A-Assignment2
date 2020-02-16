2918e69937e685d4cd0d93d8f87cdac9ae9812fd
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2020-02-15 22:06:10 -0500

    time conversion in playground.js

---
+++ js/playground.js
@@ -0,0 +1,44 @@
+
+
+function getDays(time_code) {
+    let timecode = time_code;
+    let ans = parseInt(timecode/(24*60*60) + 1).toString();
+    ans = ans.length == 1 ? "0" + ans : ans;
+    return ans;
+}
+
+function getHours(time_code) {
+    let timecode = time_code;
+    timecode -= (getDays(timecode)-1) * 24 * 60 * 60;
+    let ans = parseInt(timecode/(60*60)).toString();
+    ans = ans.length == 1 ? "0" + ans : ans;
+    return ans;
+}
+
+function getMinutes(time_code) {
+    let timecode = time_code;
+    timecode -= (getDays(timecode)-1) * (24 * 60 * 60) + getHours(timecode) * (60 * 60)
+    let ans = parseInt(timecode/60).toString();
+    ans = ans.length == 1 ? "0" + ans : ans;
+    return ans;
+}
+
+function getSeconds(time_code) {
+    let timecode = time_code;
+    timecode -= (getDays(timecode)-1) * (24 * 60 * 60) + getHours(timecode) * (60 * 60) + getMinutes(timecode) * (60);
+    let ans = timecode.toString();
+    ans = ans.length == 1 ? "0" + ans : ans;
+    return ans;
+}
+
+
+function getDateTime(time_code) {
+    let date_time = "01/" + getDays(time_code) + "/2014 " + getHours(time_code) + ":" + getMinutes(time_code) + ":" + getSeconds(time_code);
+    return date_time;
+}
+
+let time_code = 458407;
+console.log(getDateTime(time_code))
+
+
+
